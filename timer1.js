4abc0a497c12302088f380c8eccf260a7256925b
Chris De Asis <138704176+cde-as@users.noreply.github.com>
2023-12-13 10:51:51 -0800

    timer is functional but will still beep on negative numbers. I see the error now on line 11

--- timer1.js
+++ timer1.js
@@ -1,18 +1,21 @@
-const timer = function () {
+const timer = function() {
   const time = process.argv.slice(2).map((arg) => parseInt(arg)); // Removes first 2 lines from node and returns the integer values (unsorted).
 
-  if (time <= 0 || typeof time !== "number") {
+  if (time.length === 0) {
     process.exit(1);
   }
 
   const sortedTime = time.sort((a, b) => a - b); //Input will be sorted
-  process.stdout.write("\x07");
+  
   for (let i = 0; i < sortedTime.length; i++) {
+    if (i < 0) {
+      process.exit(1);
+    }
     setTimeout(() => {
       process.stdout.write("\x07"); // BEEP
-      process.stdout.write("\x07");
-    }, sortedTime[i]);
+
+    }, sortedTime[i] * 1000);
   }
 };
 
-timer(2,3,4);
+timer();
