dcb45983e313f7ef1287aaa0b188ef796d11379c
Chris De Asis <138704176+cde-as@users.noreply.github.com>
2023-12-13 10:43:06 -0800

    no syntax errors but code is not yet functional

--- timer1.js
+++ timer1.js
@@ -1,16 +1,18 @@
+const timer = function () {
+  const time = process.argv.slice(2).map((arg) => parseInt(arg)); // Removes first 2 lines from node and returns the integer values (unsorted).
 
-
-for (let i = 2; i < process.argv.length; i ++) {
-  const input = parseInt(process.argv[i]);
-
-
-  if (input <= 0 || typeof(input) !== "number") {
+  if (time <= 0 || typeof time !== "number") {
     process.exit(1);
   }
 
-  if (input > 0) {
-    process.stdout.write('\x07');
-    process.stdout.write('\x07');
-    process.stdout.write('\x07');
+  const sortedTime = time.sort((a, b) => a - b); //Input will be sorted
+  process.stdout.write("\x07");
+  for (let i = 0; i < sortedTime.length; i++) {
+    setTimeout(() => {
+      process.stdout.write("\x07"); // BEEP
+      process.stdout.write("\x07");
+    }, sortedTime[i]);
   }
-}
+};
+
+timer(2,3,4);
